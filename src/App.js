49b5c14e40d245aa74e97ec425acf62643822662
bogdankol <bogdank208@gmail.com>
2022-08-30 16:48:25 +0300

    wqeqwe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  async function onClickHandler() {
+    const {data} = await axios('https://node-app-test-one.herokuapp.com/api')
+    console.log({data})
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={onClickHandler}>BUTTON</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
